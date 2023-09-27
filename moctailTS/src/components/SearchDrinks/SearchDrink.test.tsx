aeb90e551ae5d11d61216a08858278bb20a4533a
MartinaSTV <foldermate@hotmail.com>
2023-09-27 10:06:35 +0200

    test actions

--- moctailTS/src/components/SearchDrinks/SearchDrink.test.tsx
+++ moctailTS/src/components/SearchDrinks/SearchDrink.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
-import { describe, it, expect,  beforeAll, afterAll  } from 'vitest';
+import { describe, it, expect,  beforeAll, afterAll, vi  } from 'vitest';
 import SearchDrinks from "./SearchDrinks";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+import App from "../../App";
+
+// du kan göra en json fil för att gör en mall för propsen
+//whitin (a component)
+
 
 const server = setupServer(
     rest.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic', (_req, res, ctx)=>{
@@ -21,12 +26,13 @@ const server = setupServer(
 
 describe('SearchDrink component', () => {
     it('should display input', () => {
-      render(<SearchDrinks/>)
+      render(<App/>)
+      //render(<SearchDrinks  AddToDos={AddToDos}/>)
       const input = screen.getByRole('textbox')
       expect(input).toBeInTheDocument()
     });
     it('should display drinkname efter search', async() => {
-        render(<SearchDrinks/>)
+        render(<SearchDrinks AddToDos={vi.fn()}/>)
         const input = screen.getByRole('textbox')
         expect(input).toBeInTheDocument()
         const user = userEvent.setup()
@@ -38,7 +44,7 @@ describe('SearchDrink component', () => {
       });
 
     it('should contain button lägg till ToDo lista after search', async ()=>{
-        render(<SearchDrinks/>)
+        render(<SearchDrinks AddToDos={vi.fn()} />)
         const input = screen.getByRole('textbox')
         const user = userEvent.setup()
         await user.type(input, 'Afterglow')
@@ -47,7 +53,7 @@ describe('SearchDrink component', () => {
         expect( screen.getByRole('button',{name: /lägg till Todo lista/i})).toBeInTheDocument()
     })
     it('should show error message if drink dosent exist in Fetch', async () => {
-        render(<SearchDrinks/>);
+        render(<SearchDrinks AddToDos={vi.fn()}/>);
         const user = userEvent.setup();
         await user.click(screen.getByRole('button', {name: /Search/i}));
         expect( await screen.findByText('Kunde inte hitta drink', { exact: false}))
---
+++ moctailTS/src/components/ToDoList/ToDoList.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+import { describe, it, expect, vi  } from 'vitest';
+import ToDoList from "./ToDoList";
+import    SaveToDo from '../SearchDrinks/AddToDosEx.json'
+
+
+describe('test ToDoList', ()=>{
+    it('should have header ToDoList', ()=>{
+        render(<ToDoList SaveToDo={SaveToDo} setSaveToDo={vi.fn()}/>)
+        expect(screen.getByText('TodoList')).toBeInTheDocument()
+    })
+    it('should show drink in TodoList', async()=>{
+        render(<ToDoList SaveToDo={SaveToDo} setSaveToDo={vi.fn()}/>)
+        expect(screen.getByText('TodoList')).toBeInTheDocument()
+        const user = userEvent.setup()
+        const input = screen.getByRole('textbox')
+        await user.type(input, 'Afterglow')
+        const buttonSearch = screen.getByRole('button', {name: /Search/i})
+        await user.click(buttonSearch)
+        expect( screen.getByRole('button',{name: /lägg till Todo lista/i})).toBeInTheDocument() 
+        const buttonAdd = screen.getByRole('button',{name: /lägg till Todo lista/i})
+        await user.click( buttonAdd)
+        expect( screen.getByText('Stryk')).toBeInTheDocument()
+        expect(screen.getByRole('img') ).toBeInTheDocument()
+        expect( await screen.findAllByText('Afterglow'))
+    })
+
+})
